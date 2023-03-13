0ceb3eb4959431fc0828ddd7e77dd7243a3bd8b9
Ruben van der Zouw <rubenvdzouw@gmail.com>
2023-03-13 16:50:14 +0100

    Issue 3.1 #2

---
+++ Database/rubens_webshop.sql
@@ -0,0 +1,61 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Mar 13, 2023 at 02:55 PM
+-- Server version: 10.4.27-MariaDB
+-- PHP Version: 8.2.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `rubens_webshop`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `ID` int(10) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `username` varchar(40) NOT NULL,
+  `password` varchar(40) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`ID`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `ID` int(10) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ db_repository.php
@@ -0,0 +1,58 @@
+<?php
+
+function connectToDB() {
+
+$servername = "127.0.0.1";
+$username = "rubens_webshop_user";
+$password = "test1234";
+$database = "rubens_webshop";
+
+$conn = mysqli_connect($servername, $username, $password, $database);    
+
+if (!$conn) {
+    throw new Exception("Cannot establish connection with the database." . mysqli_connect_error());
+}
+return $conn;
+}
+
+function closeDB($conn){
+    mysqli_close($conn);
+}
+
+function saveNewUser($email,$name,$password) {
+    $conn = connectToDB();
+    try {
+        $name = mysqli_real_escape_string($conn, $name);
+        $email = mysqli_real_escape_string($conn, $email);
+        $password = mysqli_real_escape_string($conn, $password);        
+
+        $sql = "INSERT INTO users (email, name, password) VALUES ('$email', '$name', '$password')";
+    
+        if (!mysqli_query($conn, $sql)) {
+            throw new Exception("Query failed, SQL: " . $sql . "Error: " . mysqli_error($conn));
+        }
+    }
+    finally {
+        closeDB($conn);
+    }
+}
+
+function findUserByEmail($email){
+    $conn = connectToDB();
+    $user = NULL;
+    try {
+        $email = mysqli_real_escape_string($conn, $email);
+        $sql = "SELECT id, email, name, password FROM users WHERE email = '$email'";
+        $result = mysqli_query($conn, $sql);
+        if ($result == false) {
+            throw new Exception("Query failed, SQL: " . $sql . "Error: " . mysqli_error($conn));
+        }
+        $user = mysqli_fetch_assoc($result);
+        return $user;
+        }
+        finally {
+        closeDB($conn);
+    } 
+}
+
+?>
