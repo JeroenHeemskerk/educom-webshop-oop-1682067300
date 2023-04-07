69c0d1006208623ae0f26a4ffe02db4b5f6f606c
Ruben van der Zouw <rubenvdzouw@gmail.com>
2023-04-07 16:51:23 +0200

    Werkende dropdown op shoppagina 3.2 #4

--- webshop.php
+++ webshop.php
@@ -1,5 +1,4 @@
 <?php
-
 $head = 'Webshop';
 
 function showWebshopHeader() {
@@ -7,22 +6,42 @@ function showWebshopHeader() {
 
 }
 
-function showWebshopContent($data) {
-    foreach($data['products'] as $key => $value){ 
+function showWebshopContent($products) {
+    foreach($products as $key => $value){ 
         showProduct($key, $value);
       }
 }
 
 function showProduct($key, $value){
+$price_id = "";
+    echo '<div class="products">';
     echo '<div class="productroster">';
     echo '<a href="index.php?page=detail&id=' . $value["id"] . '">';
     echo '<img src="Images/'. $value["image"] . '" " alt="' . $value["name"] . '" class="img">';
     echo '<h2>' . $value["name"] . '</h2>';  
     echo '</a>';
-    echo '<h3>&#8364; '.$value["price"].'</h3>';
+    echo '<br><label for="flavour">Keuze</label><br>' . PHP_EOL;
+    echo '<select id="flavour" name="flavouroptions" >' . PHP_EOL;
+    // echo '<span class="error"> ' . $data['' . $field . 'Err'] . ' </span><br>' . PHP_EOL;
+    foreach($value['flavours'] as $key => $flavour) {
+        echo '<option value="' .$flavour['material_id'] . $flavour['size_id'] . $flavour['price_id'] . '">
+                Maat: ' . $flavour['size'] . ' Materiaal: ' . $flavour['material'] . ' Prijs: &#8364;' . $flavour['price'] . '
+              </option>' . PHP_EOL; 
+    }   echo '</select>' . PHP_EOL;
+    
     echo '</div>';
-
-}
+    
+} 
+    // if ($price_id == "") {
+    //     $price_id = $flavour['price_id'];
+    // }
+    //  onchange="window.location=this.value"
+    // echo '<span class="error"> ' . $data['' . $field . 'Err'] . ' </span><br>' . PHP_EOL;            
+    // if (getUrlVar('pid') != "") { 
+    //     $price_id = getUrlVar('pid');
+    // }  
+    // echo '<h3>&#8364; ' . $value["flavours"][$price_id]["price"] . '</h3>';
+    // ' . $flavour['size_id'] . $flavour['material_id'] . $flavour['price_id'] .'
 
 function showProductDetail($id) {
     $product = doesProductExist($id);
