3dfa7082a15a771c88dae4dec24152caf52ea351
Ruben van der Zouw <rubenvdzouw@gmail.com>
2023-04-25 11:51:07 +0200

    4.1B #3

---
+++ views/home_doc.php
@@ -0,0 +1,12 @@
+<?php
+include_once 'basic_doc.php';
+
+    class HomeDoc extends BasicDoc {
+        protected function showContent() {		
+            echo '
+                <h1>Welkom<h1>
+                   <p>
+                    Welkom op de eerste website gemaakt door Ruben van der Zouw.
+                   </p>';
+        }
+    }
